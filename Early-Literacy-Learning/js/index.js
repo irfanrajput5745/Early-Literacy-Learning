1c4f03112f04c2c98e1c71a4711c502610b0eeb1
Muhammad Irfan <137328814+irfanrajput5745@users.noreply.github.com>
2024-03-19 06:50:32 +0500

    first commit

---
+++ Early-Literacy-Learning/js/index.js
@@ -0,0 +1,30 @@
+
+
+    $(".home-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
